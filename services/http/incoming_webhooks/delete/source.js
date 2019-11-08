4618b2bd17f3577664ac67130b9dca5b0e2a3c41
Dmytro Morozov <dmytro.morozov@check24.de>
2019-11-08 15:15:55 +0100

    Add mongodb-atlas service

--- services/http/incoming_webhooks/delete/source.js
+++ services/http/incoming_webhooks/delete/source.js
@@ -1,6 +1,6 @@
 // This function is the webhook's request handler.
 exports = async function(payload, response) {
-    const db = context.services.get("mongodb-atlas").db("test");
-    await db.dropDatabase();
+    const collection = context.services.get("mongodb-atlas").db("test").collection("timestamps");
+    await collection.deleteMany({});
     return  "Hello World!";
 };
